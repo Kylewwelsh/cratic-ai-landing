6c51057b4e674cf2621e2462a3a6fb9ad148ecad
kylewwelsh <kyle@kdmdental.com>
2021-05-28 19:30:03 -0600

    section one and two roughed in

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slideContainer");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "contents";  
+  dots[slideIndex-1].className += " active";
+}
+
+prev.addEventListener("click", () => {
+  plusSlides(-1);
+});
+
+next.addEventListener("click", () => {
+  plusSlides(1);
+});
