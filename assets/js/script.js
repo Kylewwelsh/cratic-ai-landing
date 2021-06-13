d61ab095e734f232a08dc1a0fedcb51c39bdb1aa
kylewwelsh <kyle@kdmdental.com>
2021-06-13 11:01:00 -0600

    updates to movie, nav bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,48 @@
-let prev = document.querySelector(".prev");
-let next = document.querySelector(".next");
+// let prev = document.querySelector(".prev");
+// let next = document.querySelector(".next");
 
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slideContainer");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("slideContainer");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";  
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "contents";  
+//   dots[slideIndex-1].className += " active";
+// }
+
+// prev.addEventListener("click", () => {
+//   plusSlides(-1);
+// });
+
+// next.addEventListener("click", () => {
+//   plusSlides(1);
+// });
+
+var myNav = document.querySelector('.nav-container');
+
+window.onscroll = function() {
+  "use strict";
+  if (document.body.scrollTop >= 600 || document.documentElement.scrollTop >= 600) {
+    myNav.classList.add("nav-scroll");
+  } else {
+    myNav.classList.remove("nav-scroll");
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "contents";  
-  dots[slideIndex-1].className += " active";
-}
-
-prev.addEventListener("click", () => {
-  plusSlides(-1);
-});
-
-next.addEventListener("click", () => {
-  plusSlides(1);
-});
+};
